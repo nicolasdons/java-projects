68d1120968bd223a88770e225145bf00d6a8b56a
nicolasdons <nicolasnssoares@gmail.com>
2026-07-27 16:03:40 -0300

    Add files via upload
    Criação projeto taxação imposto

---
+++ ProjetoJava/src/Conta.java
@@ -0,0 +1,14 @@
+public class Conta {
+    String nome;
+    Double senha;
+    String cpf;
+
+
+    public Conta(String nome, Double senha, String cpf){
+        this.nome = nome;
+        this.senha = senha;
+        this.cpf = cpf;
+    }
+}
+
+
